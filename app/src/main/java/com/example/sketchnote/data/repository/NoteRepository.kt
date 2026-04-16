0e67f524f66a17a8796e8bdd296d8cc600051d53
bichtuyen24 <duongbichtuyen2411@gmail.com>
2026-04-16 20:36:50 +0700

    Update NoteRepository.kt

--- app/src/main/java/com/example/sketchnote/data/repository/NoteRepository.kt
+++ app/src/main/java/com/example/sketchnote/data/repository/NoteRepository.kt
@@ -1,5 +1,4 @@
 package com.example.sketchnote.data.repository
-
 import android.content.Context
 import com.example.sketchnote.data.local.SketchNoteDatabase
 import com.example.sketchnote.data.local.dao.ChecklistItemDao
@@ -9,7 +8,6 @@ import com.example.sketchnote.data.local.entity.NoteEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
-
 @Singleton
 class NoteRepository @Inject constructor(
     private val dao: NoteDao,
